742162dc0a5b6ea8310a25f4860963cbeb17221a
Ariel Santos <as-ariel@outlook.com>
2017-11-15 04:30:06 -0200

    Básicos consertados

--- assets/js/001.js
+++ assets/js/001.js
@@ -1,10 +1,12 @@
 // By: h01000110 (hi)
 // github.com/h01000110
+// Contribution: asantos07
+// github.com/asantos07
 
 var max = document.getElementsByClassName("btn")[1];
 var min = document.getElementsByClassName("btn")[2];
 
-function maximize () {
+var maximize = function () {
 	var post = document.getElementsByClassName("content")[0];
 	var cont = document.getElementsByClassName("post_content")[0];
 	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
@@ -21,16 +23,17 @@ function maximize () {
 	}
 }
 
-function minimize () {
+var minimize = function () {
 	var post = document.getElementsByClassName("content")[0];
 	var cont = document.getElementsByClassName("post_content")[0];
 	var wid = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName("body")[0].clientWidth;
 
-	if ( wid > 900 ) {
+	if (wid > 900) {
 		post.style.width = "800px";
 		cont.style.width = "98.5%";
 	}
 }
-
-max.addEventListener('click', maximize, false);
-min.addEventListener('click', minimize, false);
+if (max !== undefined)
+	max.addEventListener('click', maximize, false);
+if (min !== undefined)
+	min.addEventListener('click', minimize, false);
